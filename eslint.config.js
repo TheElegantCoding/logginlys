50cea0e78acce83bf164f9b9c427ef539f6a2528
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-10 16:02:32 -0400

    feat: add ESLint configuration for code linting and style enforcement

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslintConfig from 'eslint-config-universal-code';
+
+const config = eslintConfig({
+  astro: true,
+  json: true,
+  stylistic: true,
+  unicorn: true,
+  yml: true,
+  perfectionist: true,
+  typescript: true,
+  ignore: ['postcss.config.cjs', 'dev-dist']
+}, {
+  rules: {
+    'no-console': 'off',
+  }
+});
+
+export default config;
