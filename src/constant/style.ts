6c0094ed91d5bdd6faeca7c3ad5b6b599cc833bd
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-13 19:06:45 -0400

    feat: add ANSI style constants for text formatting

---
+++ src/constant/style.ts
@@ -0,0 +1,12 @@
+const ansiStyle = {
+  reset: '\u001b[0m',
+  underline: '\u001b[4m',
+  bold: '\u001b[1m',
+  dim: '\u001b[2m',
+  italic: '\u001b[3m',
+  inverse: '\u001b[7m',
+  hidden: '\u001b[8m',
+  strikethrough: '\u001b[9m'
+};
+
+export { ansiStyle };
