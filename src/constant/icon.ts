6d108df8eecbe18ea8228c7897d853d587d310b2
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-13 19:06:15 -0400

    feat: add icon constants for logging and status indicators

---
+++ src/constant/icon.ts
@@ -0,0 +1,12 @@
+const icon = {
+  check: '\uf00c',
+  error: '\uf00d',
+  info: '\uf05a',
+  warning: '\uf071',
+  debug: '\ued7b',
+  http: '\udb81\udd9f',
+  log: '\uf03a',
+  rocket: '\uf135'
+};
+
+export { icon };
