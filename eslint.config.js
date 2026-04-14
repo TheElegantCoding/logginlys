210821a67e8a03763a35c254440c0d2725092526
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-13 20:45:20 -0400

    feat: update eslint configuration by removing unused 'astro' option and cleaning up ignored files

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 import eslintConfig from 'eslint-config-universal-code';
 
 const config = eslintConfig({
-  astro: true,
   json: true,
   stylistic: true,
   unicorn: true,
   yml: true,
   perfectionist: true,
   typescript: true,
-  ignore: ['postcss.config.cjs', 'dev-dist']
+  ignore: ['postcss.config.cjs']
 }, {
   rules: {
     'no-console': 'off',
