e3a8c3bb2a56a6d8d68fa21936c1c9c239945207
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-14 22:47:37 -0400

    feat: add showTimestamp option to LoggerLoader for enhanced loading feedback

--- src/util/loader.ts
+++ src/util/loader.ts
@@ -1,5 +1,6 @@
 import { colorAnsi } from '@src/constant/color.js';
 import { icon } from '@src/constant/icon.js';
+import { getTime } from '@src/util/time.js';
 import { isNode } from '@src/util/validation.js';
 
 import type { LoggerLoaderParameters } from '@src/type/logger_options.js';
@@ -22,25 +23,33 @@ class LoggerLoader {
   private frameIndex = 0;
   private readonly message: string = 'Loading...';
   private readonly position: 'left' | 'right' = 'left';
+  private readonly showTimestamp: boolean = false;
   private timer: null | NodeJS.Timeout = null;
 
-  constructor ({ message, position, color }: LoggerLoaderParameters) {
+  constructor ({
+    message,
+    position,
+    color,
+    showTimestamp
+  }: LoggerLoaderParameters) {
     this.message = message;
     this.position = position ?? this.position;
     this.color = color ?? this.color;
+    this.showTimestamp = showTimestamp ?? this.showTimestamp;
   }
 
   public start () {
     if (!isNode) { return; }
 
+    const time = this.showTimestamp ? `${colorAnsi.gray}${getTime()}${colorAnsi.reset} ` : '';
     process.stdout.write('\u001b[?25l');
 
     this.timer = setInterval(() => {
       const frame = spinnerFrames[this.frameIndex];
       if (this.position === 'left') {
-        process.stdout.write(`\r${this.color}${frame}${colorAnsi.reset} ${this.message}`);
+        process.stdout.write(`\r${time}${this.color}${frame}${colorAnsi.reset} ${this.message}`);
       } else {
-        process.stdout.write(`\r${this.message}${this.color} ${frame}${colorAnsi.reset}`);
+        process.stdout.write(`\r${time}${this.message}${this.color} ${frame}${colorAnsi.reset}`);
       }
 
       this.frameIndex = (this.frameIndex + 1) % spinnerFrames.length;
@@ -52,10 +61,11 @@ class LoggerLoader {
       clearInterval(this.timer);
       this.timer = null;
     }
+    const time = this.showTimestamp ? `${colorAnsi.gray}${getTime()}${colorAnsi.reset} ` : '';
     if (position === 'left') {
-      process.stdout.write(`\r\u001b[K${this.color}${symbol}${colorAnsi.reset} ${finalMessage ?? this.message}\n`);
+      process.stdout.write(`\r\u001b[K${time}${this.color}${symbol}${colorAnsi.reset} ${finalMessage ?? this.message}\n`);
     } else {
-      process.stdout.write(`\r\u001b[K${finalMessage ?? this.message} ${this.color}${symbol}${colorAnsi.reset}\n`);
+      process.stdout.write(`\r\u001b[K${time}${finalMessage ?? this.message} ${this.color}${symbol}${colorAnsi.reset}\n`);
     }
     process.stdout.write('\u001b[?25h');
   }
